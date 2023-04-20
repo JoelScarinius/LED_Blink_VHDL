b55c98649310202d9d7c84ac4bd1d36ee20d8e53
Joel <anjo19go@student.ju.se>
2023-04-20 21:05:03 +0200

    Finalized version of LED Blink asignment

--- Task1/Lab_Task1.vhd
+++ Task1/Lab_Task1.vhd
@@ -5,10 +5,9 @@ library ieee;
 entity Lab_Task1 is
 port(
 	clock_50 : in std_logic; -- 50 MHz clock
-	sw : in std_logic_vector(9 downto 0);
-	key_n : in std_logic_vector(3 downto 0);
-	ledr : out std_logic_vector(9 downto 0);
-	ledg : out std_logic_vector(7 downto 0)
+	key_n    : in std_logic_vector(1 downto 0);
+	ledr     : out std_logic_vector(9 downto 0);
+	ledg     : out std_logic_vector(7 downto 0)
 	);
 end entity Lab_Task1;
 
@@ -20,15 +19,14 @@ architecture rtl of Lab_Task1 is
 	-- Internal signal declarations
 	signal reset_n :std_logic;
 	signal counter_500ms : integer range 0 to c_500ms_count_max;
-	signal tick : std_logic;
+	signal clock_50 : std_logic;
 	signal led : std_logic;
 begin
 	-- Assign internal signals from entity inputs
 	-- Active low reset reset from KEY0
 	reset_n <= key_n(0);
 	
-	-- turn off unused LED outputs
-	--	ledr <= (others => '0');
+	-- Assign internal signals to entity outputs
 	ledr <= (others => led);
 	ledg <= (others => not led);
 	
@@ -46,15 +44,13 @@ begin
 			end if;
 		end if;
 	end process p_tick;
-	p_toggle : process(tick, reset_n)
+	p_toggle : process(clock_50, reset_n)
 	begin
 		if reset_n = '0' then
 			led <= '0';
-		elsif rising_edge(tick) then
-			if led = '1' then
-				led <= '0';
-			else 
-				led <= '1';
+		elsif rising_edge(clock_50) then
+			if tick = '1' then
+				led <= not(led);
 			end if;
 		end if;
 	end process p_toggle;
